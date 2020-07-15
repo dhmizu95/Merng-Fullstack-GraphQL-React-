222c355c68821f7d11573b12e6b75de54f7871c2
dhmizu95 <dhmizu95@gmail.com>
2020-07-16 00:33:02 +0600

    Client - Implement Apollo client, navbar, recent posts

---
+++ client/src/components/PostCard.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Button, Card, Image, Icon, Label } from 'semantic-ui-react';
+import momemt from 'moment';
+import { Link } from 'react-router-dom';
+
+const PostCard = ({ post }) => {
+	const {
+		id,
+		username,
+		body,
+		createdAt,
+		likeCount,
+		commentCount,
+		likes,
+		user: { avatar },
+	} = post;
+
+	const likePost = () => {
+		console.log('like post');
+	};
+
+	const commentPost = () => {
+		console.log('comment post');
+	};
+
+	return (
+		<Card fluid>
+			<Card.Content>
+				<Image floated='right' size='mini' src={avatar} />
+				<Card.Header>{username}</Card.Header>
+				<Card.Meta as={Link} to={`/post/${id}`}>
+					{momemt(createdAt).fromNow(true)}
+				</Card.Meta>
+				<Card.Description>{body}</Card.Description>
+			</Card.Content>
+			<Card.Content extra>
+				<Button as='div' labelPosition='right' onClick={likePost}>
+					<Button color='teal' basic>
+						<Icon name='heart' />
+					</Button>
+					<Label basic color='teal' pointing='left'>
+						{likeCount}
+					</Label>
+				</Button>
+				<Button as='div' labelPosition='right' onClick={commentPost}>
+					<Button color='blue' basic>
+						<Icon name='comments' />
+					</Button>
+					<Label basic color='blue' pointing='left'>
+						{commentCount}
+					</Label>
+				</Button>
+			</Card.Content>
+		</Card>
+	);
+};
+
+export default PostCard;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { gql, useQuery } from '@apollo/client';
+import { Grid } from 'semantic-ui-react';
+import PostCard from './../components/PostCard';
+
+const Home = () => {
+	const { loading, data: { getPosts: posts } = {} } = useQuery(
+		FETCH_POSTS_QUERY
+	);
+
+	return (
+		<Grid columns={3}>
+			<Grid.Row className='page-title'>
+				<h1>Recent Posts</h1>
+			</Grid.Row>
+			<Grid.Row>
+				{loading ? (
+					<h1>Loading Posts....</h1>
+				) : (
+					posts &&
+					posts.map((post) => (
+						<Grid.Column key={post.id} style={{ marginBottom: '20px' }}>
+							<PostCard post={post} />
+						</Grid.Column>
+					))
+				)}
+			</Grid.Row>
+		</Grid>
+	);
+};
+
+const FETCH_POSTS_QUERY = gql`
+	{
+		getPosts {
+			id
+			username
+			body
+			createdAt
+			likeCount
+			commentCount
+			likes {
+				username
+			}
+			comments {
+				id
+				username
+				body
+				createdAt
+			}
+			user {
+				avatar
+			}
+		}
+	}
+`;
+
+export default Home;
